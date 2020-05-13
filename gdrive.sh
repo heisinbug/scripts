66d18fca6f4e54e24eb4e52abfb81316d77f526f
Aryan Gupta <guptaaryan189@gmail.com>
2020-05-13 11:44:53 +0530

    Update gdrive.sh

--- gdrive.sh
+++ gdrive.sh
@@ -1,6 +1,7 @@
 git clone https://github.com/SonalSingh18/Google-drive.git -b gdrive G
 cd G
 sudo mv gdrive /home/ubuntu
+cd /home/ubuntu
 chmod +x gdrive
 sudo install gdrive /usr/local/bin/gdrive
 gdrive list
