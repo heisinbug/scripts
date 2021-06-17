0040bce894fc0edaa5db1a8a1848c4ad82f612ec
Aryan Gupta <guptaaryan189@gmail.com>
2021-06-18 00:02:24 +0530

    rclone is there !!

--- gdrive.sh
+++
@@ -1,7 +0,0 @@
-git clone https://github.com/SonalSingh18/Google-drive.git -b gdrive G
-cd G
-sudo mv gdrive /home/ubuntu
-cd /home/ubuntu
-chmod +x gdrive
-sudo install gdrive /usr/local/bin/gdrive
-gdrive list
