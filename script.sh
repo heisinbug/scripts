b0a90b0ef28608de8465ac8aeabe9c6fe20fcafa
Gupta <aryan.tushar.gupta@accenture.com>
2022-07-06 11:51:02 +0530

    github-demo-commit

--- script.sh
+++ script.sh
@@ -1,1 +1,1 @@
-sudo apt update && sudo apt upgrade && sudo apt-get install git-core && git clone https://github.com/akhilnarang/scripts && cd scripts && bash setup/android_build_env.sh && cd 
+sudo apt update && sudo apt upgrade && sudo apt-get install git-core && git clone https://github.com/akhilnarang/scripts && cd scripts && bash setup/android_build_env.sh && cd /home
